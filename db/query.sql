1862fd7ff09a960e4b4d9ba32a29466171edf2ad
lucianesl <luciane.silva@msn.com>
2017-09-28 20:08:21 -0300

    query do banco adicionada

--- db/query.sql
+++ db/query.sql
@@ -5,16 +5,20 @@ default collate utf8_general_ci;
 
 use pds;
 
+drop table usuarios;
+
 /* cria tabela de usuarios */
-create table users (
-	id int not null auto_increment,
-    cnpj varchar(11) not null unique,
-    email varchar(50) not null unique,
-    pass varchar(20) not null,
+create table usuarios (
+	id int(11) not null auto_increment,
+    cnpj varchar(18) not null unique,
+    nome varchar(256) not null,
+    email varchar(256) not null unique,
+    senha varchar(256) not null,
     primary key (id)
 ) default charset = utf8;
 
-insert into users values
-(default, 'luciane.silva@msn.com', 'senha');
+insert into usuarios values
+(default, '00.000.000/0001-00', 'Trash Sofware', 'luciane.silva@msn.com', 'senha');
 
-SELECT * FROM users WHERE email = 'luciane.silva@msn.com' AND pass = 'senha';
+SELECT * FROM usuarios WHERE email = 'luciane.silva@msn.com' AND senha = 'senha';
+SELECT * FROM usuarios;
